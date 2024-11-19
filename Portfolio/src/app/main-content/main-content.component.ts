50c1a7074b0a65caa463f5cb27d391fb633e831b
BastianThoma <ossi_1993@hotmail.de>
2024-11-19 16:38:32 +0100

    imported all needed components to main-content, linked them as well.
    All working,

--- Portfolio/src/app/main-content/main-content.component.ts
+++ Portfolio/src/app/main-content/main-content.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { LandingPageComponent } from '../landing-page/landing-page.component';
+import { AboutMeComponent } from '../about-me/about-me.component';
+import { SkillsComponent } from '../skills/skills.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { FeedbackComponent } from '../feedback/feedback.component';
+import { ContactComponent } from '../contact/contact.component';
+import { FooterComponent } from '../shared/footer/footer.component';
+
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [
+    NavbarComponent,
+    LandingPageComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    FeedbackComponent,
+    ContactComponent,
+    FooterComponent,
+  ],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  styleUrl: './main-content.component.scss',
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
