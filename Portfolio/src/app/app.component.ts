e7ec404faf4aa9253a77cc3608355570eac4837f
BastianThoma <ossi_1993@hotmail.de>
2024-11-19 17:55:24 +0100

    changed Profject structure.

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -2,13 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MainContentComponent,
+    NavbarComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portfolio';
