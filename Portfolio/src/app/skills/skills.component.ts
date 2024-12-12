cd077bdc651c70bc0f9087fe499f8ed99386054f
BastianThoma <ossi_1993@hotmail.de>
2024-12-12 13:10:44 +0100

    added many icons

--- Portfolio/src/app/skills/skills.component.ts
+++ Portfolio/src/app/skills/skills.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { SectionCardComponent } from "../section-card/section-card.component";
+import { CommonModule } from '@angular/common';
+import { SectionCardComponent } from '../section-card/section-card.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [SectionCardComponent],
+  imports: [SectionCardComponent, CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
 })
 export class SkillsComponent {
-
+  skills = [
+    { icon: '/assets/img/html.png', label: 'HTML' },
+    { icon: '/assets/img/css.png', label: 'CSS' },
+    { icon: '/assets/img/javascript.png', label: 'JavaScript' },
+    { icon: '/assets/img/typescript.png', label: 'TypeScript' },
+    { icon: '/assets/img/angular.png', label: 'Angular' },
+    { icon: '/assets/img/firebase.png', label: 'Firebase' },
+    { icon: '/assets/img/material_design.png', label: 'Material Design' },
+    { icon: '/assets/img/git.png', label: 'Git' },
+    { icon: '/assets/img/api.png', label: 'API' },
+    { icon: '/assets/img/python.png', label: 'Python' },
+    { icon: '/assets/img/scrum.png', label: 'Scrum' },
+  ];
 }
