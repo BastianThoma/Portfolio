849b5d6064b234c4952e039f5f009ef4019532c9
BastianThoma <ossi_1993@hotmail.de>
2024-12-13 16:21:09 +0100

    added the Portfolio section, next steps in this section coming soon

--- Portfolio/src/app/projects/projects.component.ts
+++ Portfolio/src/app/projects/projects.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-projects',
   standalone: true,
@@ -8,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+
+
 
 }
