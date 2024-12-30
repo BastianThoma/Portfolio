4648e57df81152c5a98509b72191ed1344d64a0c
BastianThoma <ossi_1993@hotmail.de>
2024-12-30 22:21:13 +0100

    added burger menu with popup and started overall responsiveness

--- Portfolio/src/app/shared/navbar/navbar.component.ts
+++ Portfolio/src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  isPopupVisible = false;
+  isMenuOpen = false;
 
+  togglePopup() {
+    this.isPopupVisible = !this.isPopupVisible;
+    this.isMenuOpen = !this.isMenuOpen;
+    document.body.style.overflow = this.isPopupVisible ? 'hidden' : '';
+  }
+
+  closePopup() {
+    this.isPopupVisible = false;
+    this.isMenuOpen = false;
+    document.body.style.overflow = '';
+  }
 }
