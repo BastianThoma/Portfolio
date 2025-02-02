c59a56c0f68fdd0ad5142550e92ac47e13d5e442
BastianThoma <ossi_1993@hotmail.de>
2025-02-02 18:31:01 +0100

    many fixes and changes

--- Portfolio/src/app/app.routes.ts
+++ Portfolio/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { PoliciesComponent } from './policies/policies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainContentComponent},
+    { path: 'imprint', component: PoliciesComponent}
+];
---
+++ Portfolio/src/app/policies/policies.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-policies',
+  standalone: true,
+  imports: [ImprintComponent, PrivacyPolicyComponent, TranslateModule],
+  templateUrl: './policies.component.html',
+  styleUrl: './policies.component.scss'
+})
+export class PoliciesComponent {
+  constructor(private translate: TranslateService) {}
+}
