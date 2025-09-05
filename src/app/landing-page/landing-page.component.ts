e79f5ae4053f541565fb2af7df5ebacfab69c8c4
BastianThoma <ossi_1993@hotmail.de>
2025-09-05 13:51:42 +0200

    Add particles.js background to landing page
    Integrated particles.js for animated background on the landing page. Updated HTML, SCSS, and TypeScript to support the effect, and added required assets and configuration files.

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { ScrollingBannerComponent } from '../scrolling-banner/scrolling-banner.component';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { FocusService } from '../services/focus-service/focus-service';
+
+declare var particlesJS: any;
 
 @Component({
   selector: 'app-landing-page',
@@ -10,14 +12,21 @@ import { FocusService } from '../services/focus-service/focus-service';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements AfterViewInit {
   constructor(
     private translate: TranslateService,
     private focusService: FocusService
+    
   ) {}
 
   scrollToContactForm(): void {
     document.getElementById('contactForm')?.scrollIntoView({ behavior: 'smooth' });
     this.focusService.triggerFocus();
+  }
+
+    ngAfterViewInit(): void {
+    particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
   }
 }
